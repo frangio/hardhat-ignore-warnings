54b951a70f1a72298656599a91092f8a58e73f1e
Francisco Giordano <fg@frang.io>
2026-01-21 15:08:43 -0300

    fix type extensions

--- src/type-extensions.ts
+++ src/type-extensions.ts
@@ -1,6 +1,4 @@
-// To extend one of Hardhat's types, you need to import the module where it has been defined, and redeclare it.
 import 'hardhat/types/config';
-import 'hardhat/types/runtime';
 
 import type { WarningId } from './error-codes.d.ts';
 
